1a1317f254b55b1f3635f7a42538cedb6ca1146d
xueshan.wei <xueshan.wei@mljr.com>
2017-09-26 18:01:12 +0800

    update---添加根据ID查询单个account

--- account/src/main/java/com/iag/bbscloud/account/model/repository/AccountRepository.java
+++ account/src/main/java/com/iag/bbscloud/account/model/repository/AccountRepository.java
@@ -3,11 +3,13 @@ package com.iag.bbscloud.account.model.repository;
 import com.iag.bbscloud.account.model.Account;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigInteger;
+
 /**
  * @Author xueshan.wei@mljr.com
  * @Date 2017/9/25 下午3:24
  */
-public interface AccountRepository extends CrudRepository<Account, String> {
+public interface AccountRepository extends CrudRepository<Account, BigInteger> {
 
     Account queryAccountByUidAndName(String uid, String name);
 
